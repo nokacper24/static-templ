9c382a9a1588af3ae884901485fb82ec525d2295
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-05-30 11:34:41 +0200

    feat: remove all dist files before generating, remove dead code and comments

--- main.go
+++ main.go
@@ -22,13 +22,11 @@ func main() {
 	if err != nil {
 		log.Fatal("Error finding module name:", err)
 	}
-	log.Println(modName)
 
 	dirs, err := finder.FindDirsWithGoFiles(inputPath)
 	if err != nil {
 		log.Fatal("Error finding go dirs:", err)
 	}
-	log.Println(dirs)
 
 	funcs, err := finder.FindFuncsToCall(dirs)
 	if err != nil {
@@ -37,51 +35,20 @@ func main() {
 
 	importsMap := map[string]bool{}
 	for _, f := range funcs {
-		// log.Println(f.FuncSign())
-		// log.Println(f.HtmlFileName())
 		importsMap[f.Location("")] = true
-		fmt.Println(f.Location(""))
-		fmt.Println(f.ToGenerate(inputPath, dirPath))
 	}
 	var importsSlice []string
 	for imp := range importsMap {
 		importsSlice = append(importsSlice, fmt.Sprintf("%s/%s", modName, imp))
 	}
 
+	err = os.RemoveAll(dirPath)
+	if err != nil {
+		log.Fatal("err removing files", err)
+	}
+
 	err = generator.Generate(importsSlice, funcs)
 	if err != nil {
 		log.Fatal("err generating script", err)
 	}
-
-	// myfunc := funcs[0]
-	// var funcName any = myfunc.FuncSign()
-	// if v, ok := funcName.(templ.Component); ok {
-
-	// 	fmt.Sprintf()
-	// 	f, err := os.Create()
-	// 	if err != nil {
-	// 		log.Fatal("Error creating file:", err)
-	// 	}
-	// 	v.Render(context.Background(), f)
-	// }
-
-	// var funcName2 any = pages.Index()
-
-	// ctx := context.Background()
-
-	// p := "dist/test/file.html"
-
-	// if err := os.MkdirAll(filepath.Dir(p), os.ModePerm); err != nil {
-	// 	log.Fatal("error creating dirs:", err)
-	// }
-
-	// f, err := os.Create(p)
-	// if err != nil {
-	// 	log.Fatal("error creating file:", err)
-	// }
-
-	// pages.Index().Render(ctx,f)
-
-	// f.Close()
-
 }
