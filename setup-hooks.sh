3ed34bd63e0ab8de5641efec9aa62f5cba590c30
indaco <github@mircoveltri.me>
2024-07-11 10:34:44 +0200

    fix(setup-hooks.sh): hook names

--- setup-hooks.sh
+++ setup-hooks.sh
@@ -3,7 +3,8 @@
 # Set the hooks directory to the `.githooks` folder in the repository
 git config core.hooksPath .githooks
 
-# Make the pre-commit hook executable
-chmod +x .githooks/pre-commit
+# Make the hooks executable
+chmod +x .githooks/commit-msg
+chmod +x .githooks/post-commit
 
 echo "Git hooks have been set up."
