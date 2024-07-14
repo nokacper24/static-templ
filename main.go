b012a7bea3fdb07c6bd1e750f557296e81c896fb
indaco <github@mircoveltri.me>
2024-07-14 20:26:12 +0200

    fix: run with default and revert to simple logic

--- main.go
+++ main.go
@@ -44,18 +44,15 @@ type flags struct {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		usage()
-		return
-	}
-
-	// Handle subcommands
-	switch os.Args[1] {
-	case "version", "--version":
-		handleVersionCmd()
-		return
-	default:
-		// Continue with existing flag parsing
+	if len(os.Args) > 1 {
+		switch os.Args[1] {
+		case "version", "--version":
+			handleVersionCmd()
+			return
+		default:
+			log.Printf("Unknown subcommand '%s'. Continue with the defaults", os.Args[1])
+			// Continue with existing flag parsing
+		}
 	}
 
 	// Parse command line flags
@@ -63,26 +60,47 @@ func main() {
 
 	// Prepare output directory if necessary
 	if flags.Mode == modeBundle && flags.OutputDir != flags.InputDir {
-		prepareOutputDirectory(flags.OutputDir)
+		if err := clearAndCreateDir(flags.OutputDir); err != nil {
+			log.Fatal("Error preparing output directory:", err)
+		}
 	}
 
 	// Prepare directories and find files
 	modulePath, groupedFiles := prepareDirectories(flags.InputDir)
 
 	// Run templ fmt if specified
-	runTemplFmtIfNeeded(flags.RunFormat, groupedFiles)
+	if flags.RunFormat {
+		runTemplFmt(groupedFiles)
+	}
 
 	// Run templ generate if specified
-	groupedFiles = runTemplGenerateIfNeeded(flags.RunGenerate, flags.InputDir)
+	if flags.RunGenerate {
+		groupedFiles = runTemplGenerate(flags.InputDir)
+	}
 
 	// Find functions to call in the generated Go files
 	funcs := findFunctions(groupedFiles.TemplGoFiles)
 
 	// Create output script directory
-	createOutputScriptDir()
+	if err := os.MkdirAll(outputScriptDirPath, os.ModePerm); err != nil {
+		log.Fatalf("Error creating temp dir: %v", err)
+	}
+
+	if err := copyFilesIntoOutputDir(groupedFiles.OtherFiles, flags.InputDir, flags.OutputDir); err != nil {
+		log.Fatalf("Error copying files: %v", err)
+	}
 
 	// Handle modes
-	handleMode(flags.Mode, funcs, modulePath, flags.InputDir, flags.OutputDir, groupedFiles.OtherFiles, flags.Debug)
+	switch flags.Mode {
+	case modeBundle:
+		log.Println("Operational mode: bundle")
+		handleBundleMode(funcs, modulePath, flags.InputDir, flags.OutputDir, groupedFiles.OtherFiles, flags.Debug)
+	case modeInline:
+		log.Println("Operational mode: inline")
+		handleInlineMode(funcs, modulePath, flags.InputDir, flags.Debug)
+	default:
+		log.Fatalf("Unknown mode: %s", flags.Mode)
+	}
 }
 
 // Handle the version command to display the version information
@@ -129,13 +147,6 @@ func prepareDirectories(inputDir string) (string, *finder.GroupedFiles) {
 	return modulePath, groupedFiles
 }
 
-// Run templ fmt command if needed
-func runTemplFmtIfNeeded(runFormat bool, groupedFiles *finder.GroupedFiles) {
-	if runFormat {
-		runTemplFmt(groupedFiles)
-	}
-}
-
 // Run templ fmt command
 func runTemplFmt(groupedFiles *finder.GroupedFiles) {
 	done := make(chan struct{})
@@ -147,14 +158,6 @@ func runTemplFmt(groupedFiles *finder.GroupedFiles) {
 	}()
 	<-done
 	log.Println("Completed running 'templ fmt'")
-}
-
-// Run templ generate command if needed
-func runTemplGenerateIfNeeded(runGenerate bool, inputDir string) *finder.GroupedFiles {
-	if runGenerate {
-		return runTemplGenerate(inputDir)
-	}
-	return nil
 }
 
 // Run templ generate command
@@ -187,49 +190,6 @@ func findFunctions(templGoFiles []string) []finder.FunctionToCall {
 	return funcs
 }
 
-// Create output script directory
-func createOutputScriptDir() {
-	if err := os.MkdirAll(outputScriptDirPath, os.ModePerm); err != nil {
-		log.Fatalf("Error creating temp dir: %v", err)
-	}
-}
-
-// Handle operational mode
-func handleMode(mode string, funcs []finder.FunctionToCall, modulePath, inputDir, outputDir string, otherFiles []string, debug bool) {
-	switch mode {
-	case modeBundle:
-		log.Println("Operational mode: bundle")
-		handlePagesMode(funcs, modulePath, inputDir, outputDir, otherFiles, debug)
-	case modeInline:
-		log.Println("Operational mode: inline")
-		handleComponentsMode(funcs, modulePath, inputDir, debug)
-	default:
-		log.Fatalf("Unknown mode: %s", mode)
-	}
-}
-
-// Handle bundle mode
-func handlePagesMode(funcs []finder.FunctionToCall, modulePath, inputDir, outputDir string, otherFiles []string, debug bool) {
-	if err := copyFilesIntoOutputDir(otherFiles, inputDir, outputDir); err != nil {
-		log.Fatalf("Error copying files: %v", err)
-	}
-
-	if err := generator.GenerateForBundleMode(getOutputScriptPath(), finder.FindImports(funcs, modulePath), funcs, inputDir, outputDir); err != nil {
-		log.Fatalf("Error generating script when mode=pages: %v", err)
-	}
-
-	runGeneratedScript(debug)
-}
-
-// Handle inline mode
-func handleComponentsMode(funcs []finder.FunctionToCall, modulePath, inputDir string, debug bool) {
-	if err := generator.GenerateForInlineMode(getOutputScriptPath(), finder.FindImports(funcs, modulePath), funcs, inputDir); err != nil {
-		log.Fatalf("Error generating script when mode=components: %v", err)
-	}
-
-	runGeneratedScript(debug)
-}
-
 // Run the generated script
 func runGeneratedScript(debug bool) {
 	cmd := exec.Command("go", "run", getOutputScriptPath())
@@ -247,7 +207,6 @@ func runGeneratedScript(debug bool) {
 	}
 }
 
-// Display usage information
 func usage() {
 	output := fmt.Sprintf(`Usage of %[1]v:
 %[1]v [flags] [subcommands]
@@ -300,11 +259,22 @@ func clearAndCreateDir(dir string) error {
 	return os.MkdirAll(dir, os.ModePerm)
 }
 
-// Prepare output directory by clearing and creating it
-func prepareOutputDirectory(outputDir string) {
-	if err := clearAndCreateDir(outputDir); err != nil {
-		log.Fatal("Error preparing output directory:", err)
+func handleBundleMode(funcs []finder.FunctionToCall, modulePath, inputDir, outputDir string, otherFiles []string, debug bool) {
+	if err := copyFilesIntoOutputDir(otherFiles, inputDir, outputDir); err != nil {
+		log.Fatalf("Error copying files: %v", err)
 	}
+	if err := generator.GenerateForBundleMode(getOutputScriptPath(), finder.FindImports(funcs, modulePath), funcs, inputDir, outputDir); err != nil {
+		log.Fatalf("Error generating script when mode=pages: %v", err)
+	}
+	runGeneratedScript(debug)
+}
+
+// Handle components mode
+func handleInlineMode(funcs []finder.FunctionToCall, modulePath, inputDir string, debug bool) {
+	if err := generator.GenerateForInlineMode(getOutputScriptPath(), finder.FindImports(funcs, modulePath), funcs, inputDir); err != nil {
+		log.Fatalf("Error generating script when mode=components: %v", err)
+	}
+	runGeneratedScript(debug)
 }
 
 // Copy a file from one path to another
